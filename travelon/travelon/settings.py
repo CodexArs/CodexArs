729d06b8f5ad37decd38d158d50931eb781badf1
Adarsh Shaju B <123304718+adarshshajub@users.noreply.github.com>
2025-01-11 12:02:17 +0000

    Django project update

--- travelon/travelon/settings.py
+++ travelon/travelon/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "hotels",
+    "navbar",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -54,7 +56,9 @@ ROOT_URLCONF = "travelon.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR.joinpath('templates'),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
