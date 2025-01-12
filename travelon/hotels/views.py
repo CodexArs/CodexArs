cc8c6a28fd07d36479e849c94e7f0f5fb79b17b8
adarshshajub <123304718+adarshshajub@users.noreply.github.com>
2025-01-12 23:17:47 +0530

    update the search and suggestions added location based redirection page

--- travelon/hotels/views.py
+++ travelon/hotels/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+import json
+from django.core import serializers
+from django.template import loader
 
 # Create your views here.
 
@@ -16,9 +20,34 @@ def hotelpartner(request):
         return render(request, 'navbar/search-hotels.html', { 'searchhotel':searchhotel, 'hotels':hotels })
     else :
         return render(request, 'navbar/search-hotels.html', { } )
+    
+def view_hotels(request):
+    data = serializers.serialize('json', HotelPartnerList.objects.all())
+    return HttpResponse(data, content_type='application/json')
+    
 
+def searchhotel(request):
+    name = request.GET.get('name')
+    hotellist = []
+    if name:
+        hotels =  HotelPartnerList.objects.filter(LOCATION__icontains=name)
+        for hotel in hotels:
+            hotellist.append((hotel.LOCATION))
+    return JsonResponse({'status':200, 'data': hotellist})
 
+def search(request):
+    return render(request, 'hotels/search-hotel.html')
 
+def hotel_location(request,name):
+    hotels =  HotelPartnerList.objects.filter(LOCATION__icontains=name)
+    if not hotels:
+        template = loader.get_template('hotels/error.html')
+    else:
+        template = loader.get_template('hotels/hotel-location.html')
+    context = {
+        'hotel': hotels,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 
